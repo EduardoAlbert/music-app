cd9bd39b5c340963c3933bcedb44bc10f21167f1
eduardoalbert <albert-costa1@live.com>
2023-08-20 21:34:41 -0300

    Added sidebar and login

---
+++ pages/login.js
@@ -0,0 +1,33 @@
+import { getProviders, signIn } from "next-auth/react";
+
+function login({ providers }) {
+  return (
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img className="w-52 mb-5" src="https://i.imgur.com/fPuEa9V.png" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" }
+            )}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
