65ee37fe8b531871db755262b9be4381b5f18e00
eduardoalbert <albert-costa1@live.com>
2023-08-31 19:51:49 -0300

    Updated login design

--- pages/login.js
+++ pages/login.js
@@ -2,21 +2,31 @@ import { getProviders, signIn } from "next-auth/react";
 
 function login({ providers }) {
   return (
-    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
-      <img className="w-52 mb-5" src="https://i.imgur.com/fPuEa9V.png" />
+    <>
+      <div className="absolute top-0 left-0 w-full h-full backdrop-blur-sm bg-green-950 bg-opacity-20 z-10"></div>
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute object-cover w-full h-full opacity-10 animate-pulse"
+      >
+        <source src="/party.mp4" type="video/mp4" />
+      </video>
 
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button
-            className="bg-[#18D860] text-white p-5 rounded-full"
-            onClick={() => signIn(provider.id, { callbackUrl: "/" }
-            )}
-          >
-            Login with {provider.name}
-          </button>
-        </div>
-      ))}
-    </div>
+      <div className="absolute flex flex-col items-center min-h-screen w-full justify-center z-20">
+        <img className="w-52 mb-5" src="https://i.imgur.com/fPuEa9V.png" />
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button
+              className="bg-[#18D860] text-black font-bold p-5 rounded-2xl hover:bg-[#e2ece7] transition duration-200 ease-in-out"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Login with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
