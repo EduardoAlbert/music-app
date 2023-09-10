dcc87d06980ff41484b2a3521c8aef732674400c
eduardoalbert <albert-costa1@live.com>
2023-09-10 16:28:00 -0300

    Updated default playlist id

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -7,5 +7,5 @@ export const playlistState = atom({
 
 export const playlistIdState = atom({
   key: "playlistIdState",
-  default: "10VJLvFAVUgCXvFeOzukDh",
+  default: "3eyC2yEERacrKZCASHV372",
 });
